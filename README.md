ace910994fd36a4e0bf783eb0546376f71dbbdb0
arjunamarcelino <13519021@std.stei.itb.ac.id>
2021-01-27 02:09:09 +0700

    init

---
+++ README.md
@@ -0,0 +1,57 @@
+# Tugas Kecil 1 IF2211 Strategi Algoritma - 13519021
+> Sebuah program penyelesaian *Cryptarithmetic* dengan algoritma *Brute Force*
+
+Program ini adalah program kecil (sederhana) dalam bahasa Python yang mengimplementasikan algoritma *Brute Force* untuk mencari solusi persoalan *cryptarithmetic*
+
+## Daftar Isi
+* [Info General](#info-general)
+* [Teknologi](#teknologi)
+* [Persiapan program](#persiapan-program)
+* [Cara menggunakan program](#cara-menggunakan-program)
+* [Status](#status)
+* [Kontak](#kontak)
+
+## Info General
+Tujuan dari pembuatan proyek ini adalah untuk memenuhi tugas kecil I mata kuliah IF2211 Strategi Algoritma semester 4 Teknik Informatika Institut Teknologi Bandung tahun ajaran 2020/2021.
+
+## Teknologi
+Python - version 3.9.0
+
+## Persiapan program
+1. Download Python versi 3 di https://www.python.org/downloads/ sesuai OS masing-masing. Kami menyarankan untuk mendownload Python versi 3.7 ke atas.
+2. Instal Python (Centang pilihan "*Add Python 3.X to PATH*" agar packages Python dapat diinstal melalui Command Prompt)
+3. Salin link https://github.com/dethaa/Algeo02-19021.git
+4. Pilih direktori yang ingin Anda jadikan sebagai tempat programnya dengan perintah **cd**  *nama-direktori-anda*/
+5. Klon link *git repository* dengan perintah berikut
+```bash
+git clone "https://github.com/dethaa/Algeo02-19021.git"
+``` 
+6. Buka folder *repository* tersebut dan persiapkan file yang berisi soal yang ingin diselesaikan
+7. Buka kembali Command Prompt
+8. Buka *directory* folder src pada Command Prompt dengan perintah **cd src/**
+9. Jalankan program app.py dengan perintah
+```bash
+python app.py
+```
+10. Program *My Simple Search Engine* dapat dijalankan
+
+## Cara Menggunakan Program
+
+
+## Status
+Selesai
+
+|                                Poin                                   |      Ya      |     Tidak     |
+| :-------------------------------------------------------------------- |:------------:| -------------:|
+| Program berhasil dikompilasi tanpa kesalahan (no syntax error)        |       v      |               |
+| Program berhasil running                                              |       v      |               |
+| Program dapat membaca file masukan dan menuliskan luaran.             |       v      |               |
+| Solusi cryptarithmetic hanya benar untuk persoalan cryptarihtmetic    |              |               |
+| dengan dua buah operand.                                              |              |       v       |
+| Solusi cryptarithmetic benar untuk persoalan cryptarihtmetic untuk    |              |               |
+| lebih dari dua buah operand.                                          |       v      |               |
+
+## Kontak
+Dibuat oleh:
+Arjuna Marcelino - 13519021@std.stei.itb.ac.id
+Teknik Informatika ITB
---
+++ src/main.py
@@ -0,0 +1,121 @@
+import time
+
+# fungsi untuk mencari nilai sebuah permutasi
+def permutasi (elemen_list):
+    # best case mengembalikan elemen tunggal
+    if len(elemen_list) == 1:
+        return [elemen_list]
+    # rekurens
+    else :
+        temp =[]
+        # melanjutkan ke elemen berikutnya
+        for i in range (len(elemen_list)):
+            a = elemen_list[i]
+            sisaList = elemen_list[:i] + elemen_list[i+1:]
+            for j in permutasi (sisaList):
+                temp.append([a]+j)
+        return temp
+
+# fungsi main
+print("###########################################################")
+print("#           --    --           --            --           #")
+print("#          |  |  |  |         |  |          |  |          #")
+print("#          |  |  |  |         |  |          |  |          #")
+print("#          |   --   |         |  |          |  |          #")
+print("#          |   --   |         |  |          |  |          #")
+print("#          |  |  |  |         |  |           --           #")
+print("#          |  |  |  |         |  |           /\           #")
+print("#           --    --           --            \/           #")
+print("###########################################################")
+print("#                                                         #")
+print("#                 CRYPTARITHMETIC SOLVER                  #")
+print("#                                                         #")
+print("#      ARJUNA MARCELINO         13519021        K01       #")
+print("#                                                         #")
+print("###########################################################")
+print("")
+print("File yang diterima hanya dengan ekstensi \".txt\"")
+
+# menerima masukan nama file
+soal = input("Masukkan nama file teks (dengan ekstensi) : ")
+
+# buka file
+file_soal = open("../test/"+soal,"r")
+
+# baca isi file
+baca_soal = file_soal.read()
+
+# mulai menghitung waktu
+start = time.time()
+
+# membersihkan karakter '+', '-', '\n'
+car_bersih = baca_soal
+car_bersih = car_bersih.replace('+','')
+car_bersih = car_bersih.replace('-','')
+car_bersih = car_bersih.replace('\n','')
+
+# menyimpan kata
+kata = car_bersih.split()
+
+# menyimpan huruf berbeda
+huruf = car_bersih.replace(' ','')
+huruf = list(dict.fromkeys(huruf))
+
+# menentukan banyaknya operan
+n_operan = len(kata)-1
+
+
+cek = 0
+# mencari solusi yang benar dengan mencoba semua kemungkinan permutasi
+found = False
+data = list('1234567890') 
+for p in permutasi(data): 
+    sum = 0
+    nilai_hasil = 0
+    nilai_operan = [0 for i in range (n_operan)]
+    nol = False
+    for i in range (n_operan) :
+        digit = len(kata[i])-1
+        for curr in kata[i] :
+            idx = huruf.index(curr)
+            nilai_operan[i] += int(p[idx])*(10**digit)
+            digit-=1
+        sum += nilai_operan[i]
+        if (int(p[huruf.index(kata[i][0])])==0):
+            nol = True
+
+    digit_hasil = len(kata[n_operan])-1  
+    for curr in kata[n_operan] :
+        indx = huruf.index(curr)
+        nilai_hasil += int(p[indx])*(10**digit_hasil)
+        digit_hasil-=1
+
+    if (sum == nilai_hasil) and (not (nol)) :
+            found = True
+            break
+     
+    cek +=1
+        
+end = time.time()
+# menampilkan output
+print("")
+print(baca_soal)
+
+if (not(found)):
+    print("\nTidak ada solusi untuk menyelesaikan permasalahan Cryptarithmetic ini")
+else :
+    print("\nSolusi :")
+    for i in range (n_operan-1):
+        spasi = len(kata[n_operan])-len(kata[i])
+        print(' '*spasi+str(nilai_operan[i]))
+
+    spasi = len(kata[n_operan])-len(kata[n_operan-1])
+    print(' '*spasi+str(nilai_operan[n_operan-1])+' +')
+    print('-'*(len(kata[n_operan])+2))
+    print(nilai_hasil)
+    print("\nWaktu eksekusi program : "+ str(end - start),"detik")
+    print("\nJumlah tes yang dilakukan untuk menemukan substitusi angka yang benar untuk setiap huruf : ",cek,"kali")
+    print("")
+    
+# tutup file
+file_soal.close()
